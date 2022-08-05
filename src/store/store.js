952c2f9371c3812f6f6a8c1e912a075ebb0578c7
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2022-08-05 14:19:12 +0530

    1 file modified, 1 added

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-
+import MovieReducer from './reducers/movieSlice';
 export const store = configureStore({
-  reducer: {},
+  reducer: { movies: MovieReducer },
 });
